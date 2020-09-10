68a58d04af6b7085f8ccadc726417cbb68c4d0eb
guoguo <leslie_guoguo@163.com>
2020-09-10 17:27:28 +0800

    same tree

---
+++ src/main/java/acmr/algorithm/common/TreeNode.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package acmr.algorithm.common;
+
+/**
+ * @author guohz
+ * @date 2020年9月10日
+ * @description TreeNode
+ * 二叉树
+ */
+public class TreeNode {
+
+	 public int val;
+	 public TreeNode left;
+	 public TreeNode right;
+	 public TreeNode() {}
+	 public TreeNode(int val) { this.val = val; }
+	 public TreeNode(int val, TreeNode left, TreeNode right) {
+		 this.val = val;
+		 this.left = left;
+		 this.right = right;
+	 }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
